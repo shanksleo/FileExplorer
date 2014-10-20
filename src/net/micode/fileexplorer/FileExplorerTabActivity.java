90cccab806bede4f7c5eaf514d423ddb0cd6436e
shanksleo <876143533@qq.com>
2014-10-20 16:55:35 +0800

    翻译

--- src/net/micode/fileexplorer/FileExplorerTabActivity.java
+++ src/net/micode/fileexplorer/FileExplorerTabActivity.java
@@ -123,14 +123,20 @@ public class FileExplorerTabActivity extends Activity {
 
     /**
      * This is a helper class that implements the management of tabs and all
-     * details of connecting a ViewPager with associated TabHost.  It relies on a
-     * trick.  Normally a tab host has a simple API for supplying a View or
-     * Intent that each tab will show.  This is not sufficient for switching
-     * between pages.  So instead we make the content part of the tab host
+     * details of connecting a ViewPager with associated TabHost.
+     * 这是一个帮助类实现了tab 的管理和ViewPager连接Tbahost 的所有细节
+     * It relies on a trick.  Normally a tab host has a simple API for supplying a View or
+     * Intent that each tab will show.
+     *
+     * This is not sufficient（足够） for switching between pages.
+     * So instead we make the content part of the tab host
      * 0dp high (it is not shown) and the TabsAdapter supplies its own dummy
-     * view to show as the tab content.  It listens to changes in tabs, and takes
+     * view to show as the tab content.
+     *
+     * It listens to changes in tabs, and takes
      * care of switch to the correct paged in the ViewPager whenever the selected
      * tab changes.
+     * tabs 监听到按钮按下，然后在ViewPager切换当前page。
      */
     public static class TabsAdapter extends FragmentPagerAdapter
             implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
